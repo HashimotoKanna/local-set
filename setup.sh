181cab1bffbaf2f6c5a9979f2fbf30d610bf05f6
Chansol, Han <chansol0505@naver.com>
2019-02-13 20:32:10 +0900

    u1

--- setup.sh
+++ setup.sh
@@ -5,33 +5,39 @@ echo "#######################################################"
 echo "# update and upgrade ubuntu packages                  #"
 echo "#######################################################"
 sudo apt update && sudo apt upgrade -y
+sudo apt install -y git tmux vim gdb curl zsh fonts-powerline
+curl -sL https://deb.nodesource.com/setup_10.x | sudo bash -
+sudo apt install -y nodejs
 
 # install zsh and change default shell as zsh
 echo "#######################################################"
 echo "# install zsh and change shell as zsh                 #"
 echo "#######################################################"
-sudo apt install zsh -y && chsh -s $(which zsh)
+# sudo apt install zsh -y 
+chsh -s $(which zsh)
 # install oh-my-zsh
 echo "#######################################################"
 echo "# install oh-my-zsh                                   #"
 echo "#######################################################"
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+# git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
+git clone https://github.com/denysdovhan/spaceship-prompt.git "$HOME/.oh-my-zsh/custom/themes/spaceship-prompt"
+# ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+ln -s "$HOME/.oh-my-zsh/custom/themes/spaceship-prompt/spaceship.zsh-theme" "$HOME/.oh-my-zsh/custom/themes/spaceship.zsh-theme"
 
-#echo "# Load custom aliases" >> $HOME/.zshrc
-#echo "[[ -s "$HOME/.bash_aliases" ]] && source "$HOME/.bash_aliases"" >> $HOME/.zshrc
-#echo " ZSH_THEME="dst"" >> $HOME/.zshrc
-#echo "HIST_STAMPS="mm/dd/yyyy"" >> $HOME/.zshrc
-#echo "plugins=(" >> $HOME/.zshrc
-#echo "  colored-man-pages" >> $HOME/.zshrc
-#echo "  colorize" >> $HOME/.zshrc
-#echo "  battery" >> $HOME/.zshrc
-#echo "  themes" >> $HOME/.zshrc
-#echo "  z" >> $HOME/.zshrc
-#echo ")" >> $HOME/.zshrc
-#echo "# disable Software Flow Control (XON/XOFF flow control)" >> $HOME/.zshrc
-#echo "stty -ixon" >> $HOME/.zshrc
-#echo "# battery info" >> $HOME/.zshrc
-#echo "RPROMPT='$(battery_pct_prompt)'" >> $HOME/.zshrc
+echo "# Load custom aliases" >> $HOME/.zshrc
+echo "[[ -s "$HOME/.bash_aliases" ]] && source "$HOME/.bash_aliases"" >> $HOME/.zshrc
+echo "plugins=(" >> $HOME/.zshrc
+echo "  colored-man-pages" >> $HOME/.zshrc
+echo "  colorize" >> $HOME/.zshrc
+echo "  battery" >> $HOME/.zshrc
+echo "  z" >> $HOME/.zshrc
+echo "  zsh-autosuggestions" >> $HOME/.zshrc
+echo ")" >> $HOME/.zshrc
+echo "# disable Software Flow Control (XON/XOFF flow control)" >> $HOME/.zshrc
+echo "stty -ixon" >> $HOME/.zshrc
+echo "# battery info" >> $HOME/.zshrc
+echo "RPROMPT='$(battery_pct_prompt)'" >> $HOME/.zshrc
 #echo "# Load NVM" >> $HOME/.zshrc
 #echo "export NVM_DIR="$(realpath $HOME/.nvm)"" >> $HOME/.zshrc
 #echo "[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm" >> $HOME/.zshrc
@@ -48,10 +54,12 @@ sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/i
 [ -f $HOME/.bash_aliases ] && cat dotfiles/.bash_aliases >> ~/.bash_aliases || cp dotfiles/.bash_aliases ~/.bash_aliases
 
 # install tmux
-sudo apt install tmux
+# sudo apt install tmux -y
 # set tmux configuration
 [ -f $HOME/.tmux.conf ] && cat dotfiles/.tmux.conf >> ~/.tmux.conf || cp dotfiles/.tmux.conf ~/.tmux.conf
 
+# install or update vim
+# sudo apt install vim -y
 # install vim Vundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 # set vim configuration
@@ -59,19 +67,11 @@ git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 # download jellybean awesome color theme
 curl https://raw.githubusercontent.com/nanotech/jellybeans.vim/master/colors/jellybeans.vim -o ~/.vim/colors/jellybeans.vim --create-dir
 # source .vimrc
-#vim -c PluginInstall
+# vim -c PluginInstall
 
-# install gdb
-sudo apt install gdb
-# install gdb gef
+# install gdb-gef
 wget -q -O- https://github.com/hugsy/gef/raw/master/scripts/gef.sh | sh
 # install gdb pwndbg
-# git clone https://github.com/pwndbg/pwndbg && cd pwndbg && ./setup.sh && cd ..
+ git clone https://github.com/pwndbg/pwndbg && cd pwndbg && ./setup.sh && cd ..
 # set gdb configuration
 [ -f $HOME/.gdbinit ] && cat dotfiles/.gdbinit >> ~/.gdbinit || cp dotfiles/.gdbinit ~/.gdbinit
-
-# install nvm
-#LATEST_NODE=`nvm ls-remote | tail -n 1`
-#nvm install $LATEST_NODE
-#nvm default $LATEST_NODE
-sudo npm install -g spaceship-prompt
