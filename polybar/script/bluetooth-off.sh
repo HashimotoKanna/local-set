417c3c0018fecb6764d6720918e25de4c24246c1
Chansol, Han <chansol0505@naver.com>
2019-06-26 16:25:03 +0900

    disk

--- polybar/script/bluetooth-off.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-SUDO_ASKPASS="$HOME/.config/polybar/script/zenity-pw-prompt.sh" sudo -A systemctl stop bluetooth
--- polybar/script/wifi-on.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-SUDO_ASKPASS="$HOME/.config/polybar/script/zenity-pw-prompt.sh" sudo -A ifconfig wlo1 up
-SUDO_ASKPASS="$HOME/.config/polybar/script/zenity-pw-prompt.sh" sudo -A systemctl start NetworkManager
